5c3ae47ef5408bcfff4d8d4e3b718ca54ea2eba8
hpeaker <harry_peaker@hotmail.com>
2020-06-30 11:27:42 +0100

    update dot counting

--- R/dot_counting.R
+++ R/dot_counting.R
@@ -1,5 +1,46 @@
 
 #' @export
-dot_counting <- function(n, visit = "V1", ids = NULL) {
+dot_counting <- function(n, visit = "V1", ids = NULL, visit_ids = NULL) {
+  ids <- handle_ids(n, ids)
+  visit_ids <- handle_ids(n, visit_ids)
 
+  assessment_performed <- sample_yes_no(n)
+  performed <- which(assessment_performed == "Yes")
+  not_performed <- which(assessment_performed == "No")
+
+  assessment_date <- random_date(n, which_na = not_performed)
+  reason_not_performed <- reason_not_collected(n, which_na = performed)
+
+  t1 <- sample_dotcount(n)
+  t2 <- sample_dotcount(n)
+  t3 <- sample_dotcount(n)
+  t4 <- sample_dotcount(n)
+  t5 <- sample_dotcount(n)
+  t6 <- sample_dotcount(n)
+  total <- t1 + t2 + t3 + t4 + t5 + t6
+
+
+  df <- data.frame(
+    patient_id = ids,
+    visit_id = visit_ids,
+    visit = visit,
+    dotcount_t1 = t1,
+    dotcount_t2 = t2,
+    dotcount_t3 = t3,
+    dotcount_t4 = t4,
+    dotcount_t5 = t5,
+    dotcount_t6 = t6,
+    dotcount_total = total,
+
+    assessment_performed = assessment_performed,
+    assessment_date = assessment_date,
+    reason_not_performed = reason_not_performed
+  )
+
+  return(df)
 }
+
+sample_dotcount <- function(n) {
+  sample(0:6, n, replace = TRUE)
+}
+
--- inst/sql/dot_counting.sql
+++ inst/sql/dot_counting.sql
@@ -1,3 +1,15 @@
 CREATE TABLE dot_counting(
-
+  patient_id int,
+  visit_id int,
+  visit text,
+  dotcount_t1 int,
+  dotcount_t2 int,
+  dotcount_t3 int,
+  dotcount_t4 int,
+  dotcount_t5 int,
+  dotcount_t6 int,
+  dotcount_total int.
+  assessment_performed text,
+  assessment_date date,
+  reason_not_performed text
 );
