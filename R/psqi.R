cb992df166346f083f564cb90a9b2fa6314b5324
hpeaker <harry_peaker@hotmail.com>
2020-07-28 09:03:28 +0100

    update psqi

--- R/psqi.R
+++ R/psqi.R
@@ -3,6 +3,13 @@ psqi <- function(n, visit = "V1", missing = 0.1, ids = NULL, visit_ids = NULL) {
   ids <- handle_ids(n, ids)
   visit_ids <- handle_ids(n, visit_ids)
 
+  assessment_performed <- sample_yes_no(n, prob = c(1 - missing, missing))
+  performed <- which(assessment_performed == "Yes")
+  not_performed <- which(assessment_performed == "No")
+
+  assessment_date <- random_date(n, which_na = not_performed)
+  reason_not_done <- reason_not_collected(n, which_na = performed)
+
   df <- data.frame(
     patient_id = ids,
     visit_id = visit_ids,
@@ -15,9 +22,9 @@ psqi <- function(n, visit = "V1", missing = 0.1, ids = NULL, visit_ids = NULL) {
     psqi_component_5_score,
     psqi_component_6_score,
     psqi_component_7_score,
-    psqi_item1,
+    psqi_item1 = sample_time(n, hrs = 20:23, mins = seq(0, 45, by = 15)),
     psqi_item2,
-    psqi_item3,
+    psqi_item3 = sample_time(n, hrs = c("05", "06", "07", "08", "09"), mins = seq(0, 45, by = 15)),
     psqi_item4,
     psqi_item5_a,
     psqi_item5_b,
@@ -41,10 +48,21 @@ psqi <- function(n, visit = "V1", missing = 0.1, ids = NULL, visit_ids = NULL) {
     psqi_item10_d,
     psqi_item10_e,
     psqi_item10_yesno,
-    assessment_date,
-    assessment_performed,
-    reason_not_done
-
+    assessment_date = assessment_date,
+    assessment_performed = assessment_performed,
+    reason_not_done = reason_not_done
   )
   return(df)
+}
+
+sample_time <- function(n, hrs, mins, which_na = c()) {
+  x <- paste(sample(hrs, n, replace = TRUE), sample(mins, n, replace = TRUE), sep = ":")
+  x <- remove_indices(x, which_na)
+  return(x)
+}
+
+sample_psqi_item <- function(n, which_na = c()) {
+  x <- sample(0:3, n, replace = TRUE)
+  x <- remove_indices(x, which_na)
+  return(x)
 }
