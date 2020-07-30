cbb8cd0062b7a5fb43a33eafaf3af6e004c439ef
hpeaker <harry_peaker@hotmail.com>
2020-07-30 14:48:07 +0100

    specific test for family history

--- tests/testthat/test-family_history.R
+++ tests/testthat/test-family_history.R
@@ -2,4 +2,13 @@ context("Family History")
 
 n <- 100
 
-basic_tests(family_history, n)
+#basic_tests(family_history, n)
+
+
+test_that("Sample data can be created", {
+  dat <- family_history(n)
+
+  expect_gte(nrow(dat), n)
+  expect_equal(unique(dat$patient_id), 1:n)
+})
+
